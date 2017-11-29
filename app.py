2c29e6275a46d95adacd21f03e40340cb430c1c8
Dhanunjaya Panchireddy <dhanunjaya.panchireddy@gmail.com>
2017-11-29 21:58:12 +0530

    init

--- app.py
+++ app.py
@@ -2,11 +2,25 @@ from flask import Flask
 
 app=Flask(__name__)
 
+app.config['DEBUG'] = True
+
 @app.route('/')
 @app.route('/hello')
 def hello():
 	return "<h1>Hello world!</h1>"
 
 
+@app.route('/test/<query>')
+def search(query):
+	return query
+
+
+@app.route('/name/<name>')
+def index(name):
+	if name.lower() == "dhanu":
+		return "<h1>hello, {}</h1>".format(name), 200
+	else:
+		return "Not found", 404
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
